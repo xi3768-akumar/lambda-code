07686753f0ed72f18b25a8bf4d7e5309988a4f9d
xi3768-akumar <abhishek.kumar3@xebia.com>
2023-03-02 17:26:57 +0530

    test

--- lambda/test2/test.py
+++ lambda/test2/test.py
@@ -10,6 +10,6 @@ def lambda_handler(event, context):
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
     return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    # raise Exception('Something went wrong')
     # abhishek
     # test lambda
