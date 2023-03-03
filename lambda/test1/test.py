a9d3811d8f1858e29c8e2ad7cb413be50bd54dcb
xi3807 <atul.khiste@xebia.com>
2023-03-03 12:28:53 +0530

    buildspec changed

--- lambda/test1/test.py
+++ lambda/test1/test.py
@@ -10,5 +10,6 @@ def lambda_handler(event, context):
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
     print("value4 = " + event['key4'])
+    print("value5 = " + event['key5'])
     return event['key1']  # Echo back the first key value
     print("Changes made to this lambda only")
